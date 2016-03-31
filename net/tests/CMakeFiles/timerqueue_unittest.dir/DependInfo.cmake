0919a1f39cc25cd5cfaf60250b2117ff7a817998
Jacklli <yinlong.lee@hotmail.com>
2016-03-31 20:33:40 +0800

    commit.

---
+++ net/tests/CMakeFiles/timerqueue_unittest.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/muduo-1.0.0/muduo/net/tests/TimerQueue_unittest.cc" "/root/muduo-1.0.0/muduo/net/tests/CMakeFiles/timerqueue_unittest.dir/TimerQueue_unittest.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/muduo-1.0.0/muduo/net/CMakeFiles/muduo_net.dir/DependInfo.cmake"
+  "/root/muduo-1.0.0/muduo/base/CMakeFiles/muduo_base.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
